81853a967083cba8182b044d20fa75bce0ff5bb7
karrelo <kardo.rahim@hotmail.se>
2021-02-04 14:52:59 +0100

    Third commit

---
+++ JohannasBackEnd/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using JohannasBackEnd.Domain.Models;
+using JohannasBackEnd.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JohannasBackEnd.Controllers
+{
+    public class UserController : ApiController
+    {
+        // GET: api/User
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/User/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+
+        // POST: api/User
+        public string Post([FromBody] User user)
+        {
+            bool myBool;
+            myBool = UserManager.GetInstance().CreateUser(user);
+            if (myBool == true)
+            {
+                return "successful";
+            }
+            else
+            {
+                return "unsuccessful";
+            }
+
+
+        }
+
+        // PUT: api/User/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/User/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
