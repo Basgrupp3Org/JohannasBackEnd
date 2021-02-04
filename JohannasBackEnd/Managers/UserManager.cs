452959a17a2ec0b78866dd116e4b40eea06381e3
Conny Johansson <conjoh94@outlook.com>
2021-02-04 14:08:24 +0100

    Second Commit

---
+++ JohannasBackEnd/Managers/UserManager.cs
@@ -0,0 +1,125 @@
+﻿using JohannasBackEnd.Domain;
+using JohannasBackEnd.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JohannasBackEnd.Managers
+{
+    public class UserManager
+    {
+        private static UserManager _instance;
+
+        public static UserManager GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new UserManager();
+            }
+            return _instance;
+        }
+        private UserManager()
+        {
+
+        }
+
+        public User GetUserByUserName(string userName)
+        {
+            using (var db = new MyDBContext())
+            {
+                var user = db.Users.Where(p => p.UserName == userName);
+                var person = db.Users.Find(userName);
+                return person;
+            }
+        }
+
+        public User GetUserByID(int id)
+        {
+            using (var db = new MyDBContext())
+            {
+                var user = db.Users.Find(id);
+                return user;
+            }
+        }
+
+        public bool CreateUser(User user)
+        {
+            using (var db = new MyDBContext())
+            {
+                var person = db.Users.Where(p => p.UserName == user.UserName).FirstOrDefault();
+                if (person == null)
+                {
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public void DeleteUserByID(int id)
+        {
+            using (var db = new MyDBContext())
+            {
+                var user = db.Users.Find(id);
+                db.Users.Remove(user);
+
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteUserByName(string name)
+        {
+            using (var db = new MyDBContext())
+            {
+                var user = db.Users.Where(p => p.UserName == name).FirstOrDefault();
+                if (user != null)
+                {
+                    db.Users.Remove(user);
+                }
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteUser(User user)
+        {
+            using (var db = new MyDBContext())
+            {
+                db.Users.Remove(user);
+
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdatePassword(User user, string password)
+        {
+            using (var db = new MyDBContext())
+            {
+                var person = db.Users.Where(p => p.Id == user.Id).FirstOrDefault();
+                if (person != null)
+                {
+                    person.Password = password;
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        public void UpdatePassword(string userName, string password)
+        {
+            using (var db = new MyDBContext())
+            {
+                var person = db.Users.Where(p => p.UserName == userName).FirstOrDefault();
+                if (person != null)
+                {
+                    person.UserName = password;
+                }
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
