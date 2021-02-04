32183651a2244b2d1744b6615569c4c29603c60d
MartinBiszc <biszczak@live.se>
2021-02-04 15:19:47 +0100

    fourth commit

--- JohannasBackEnd/Controllers/UserController.cs
+++ JohannasBackEnd/Controllers/UserController.cs
@@ -6,12 +6,16 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace JohannasBackEnd.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+
     public class UserController : ApiController
     {
         // GET: api/User
+
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
