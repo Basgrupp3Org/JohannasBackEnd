aad1c2c03dd852e8d532d10ff6dabf48947aa37b
karrelo <kardo.rahim@hotmail.se>
2021-02-09 14:16:20 +0100

    usernameunique

--- JohannasBackEnd/Domain/Models/User.cs
+++ JohannasBackEnd/Domain/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,7 @@ namespace JohannasBackEnd.Domain.Models
     public class User : Entity
     {
         public string Password { get; set; }
+        [StringLength(20)]
         [Index(IsUnique=true)]
         public string UserName { get; set; }
         public virtual ICollection<Balance> Balance { get; set; }
---
+++ JohannasBackEnd/Migrations/202102091313172_usernameuniq.cs
@@ -0,0 +1,20 @@
+﻿namespace JohannasBackEnd.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class usernameuniq : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Users", "UserName", c => c.String(maxLength: 20));
+            CreateIndex("dbo.Users", "UserName", unique: true);
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.Users", new[] { "UserName" });
+            AlterColumn("dbo.Users", "UserName", c => c.String());
+        }
+    }
+}
