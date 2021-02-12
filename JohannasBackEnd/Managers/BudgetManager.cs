a63f75642ef56d97a7e1c3803da7259939536bf6
Conny Johansson <conjoh94@outlook.com>
2021-02-12 18:48:38 +0100

    BudgetManager

---
+++ JohannasBackEnd/Managers/BudgetManager.cs
@@ -0,0 +1,42 @@
+﻿using JohannasBackEnd.Domain;
+using JohannasBackEnd.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JohannasBackEnd.Managers
+{
+    public class BudgetManager
+    {
+        public void CreateBudget(Budget budget)
+        {
+            using (var db = new MyDBContext())
+            {
+                db.Budgets.Add(budget);
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteBudget(int id)
+        {
+            using (var db = new MyDBContext())
+            {
+                if (db.Budgets.Find(id) != null)
+                {
+                    db.Budgets.Remove(db.Budgets.Find(id));
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        public Budget GetBudgetById(int id)
+        {
+            using (var db = new MyDBContext())
+            {
+                var budget = db.Budgets.Find(id);
+                return budget;
+            }
+        }
+    }
+}
